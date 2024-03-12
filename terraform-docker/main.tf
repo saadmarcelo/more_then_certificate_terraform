22e024cb446dcf0044b1e3effd14acea8099f252
Marcelo saad <marcelo_saad@hotmail.com>
2024-03-12 11:54:51 -0400

    docker container nodered

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -14,3 +14,13 @@ provider "docker" {
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
+
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = docker_image.nodered_image.name
+  ports {
+    internal = 1880
+    external = 1880
+  }
+
+}

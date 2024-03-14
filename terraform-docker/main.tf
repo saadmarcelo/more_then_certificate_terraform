79baa013ca75d03c248e146b9f5bbc7042d95581
Marcelo saad <marcelo_saad@hotmail.com>
2024-03-14 11:41:05 -0400

    count

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -14,22 +14,45 @@ provider "docker" {
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
+resource "random_string" "random" {
+  count   = 2
+  length  = 4
+  special = false
+  upper   = false
+}
 
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  count = 2
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.name
   ports {
     internal = 1880
-    external = 1880
+    #external = 1880
   }
-
 }
+# resource "docker_container" "nodered_container2" {
+#   name  = "nodered2"
+#   image = docker_image.nodered_image.name
+#   ports {
+#     internal = 1880
+#    #external = 1880
+#   }
+# }
 output "IP_Address" {
-value = join(":", [docker_container.nodered_container.network_data[0].ip_address, docker_container.nodered_container.ports[0].external])
-description = "The IP address and external port of the container"
+  value       = join(":", [docker_container.nodered_container[0].network_data[0].ip_address, docker_container.nodered_container[0].ports[0].external])
+  description = "The IP address and external port of the container"
 }
 
 output "container-name" {
-  value = docker_container.nodered_container.name
+  value       = docker_container.nodered_container[*].name
   description = "The name of the container"
+}
+output "IP_Address2" {
+  value       = join(":", [docker_container.nodered_container[1].network_data[0].ip_address, docker_container.nodered_container[1].ports[0].external])
+  description = "The IP address and external port of the container"
+}
+
+# output "container-name2" {
+#   value       = docker_container.nodered_container[1].name
+#   description = "The name of the container"
 }

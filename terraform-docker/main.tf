03f46e306301f6b8f6ee23ee2934052d8800bfb3
Marcelo saad <marcelo_saad@hotmail.com>
2024-05-06 11:34:57 -0400

    first module

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,21 +1,14 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-    version = "3.0.2" }
-  }
-}
-
-provider "docker" {}
-
 resource "null_resource" "dockervol" {
   provisioner "local-exec" {
     command = "mkdir noderedvol/ || true"
   }
 }
-resource "docker_image" "nodered_image" {
-  name = lookup(var.image, terraform.workspace)
+
+module "image"{ 
+  source = "./image"
+
 }
+
 resource "random_string" "random" {
   count   = local.container_count
   length  = 4
@@ -26,7 +19,7 @@ resource "random_string" "random" {
 resource "docker_container" "nodered_container" {
   count = local.container_count
   name  = join("-", ["nodered", terraform.workspace,random_string.random[count.index].result])
-  image = docker_image.nodered_image.name
+  image = module.image.image_out
   ports {
     internal = var.int_port
     external = lookup(var.ext_port, terraform.workspace)[count.index]

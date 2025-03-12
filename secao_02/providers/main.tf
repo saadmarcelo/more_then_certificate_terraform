c4c1dfdcf1b1be9463cd8177e4ae7c1d33b921d4
Marcelo saad <marcelo_saad@hotmail.com>
2025-03-12 13:23:26 -0400

    add: interpolation name

--- secao_02/providers/main.tf
+++ secao_02/providers/main.tf
@@ -1,5 +1,9 @@
+resource "random_id" "random" {
+  byte_length = 2
+
+}
 resource "github_repository" "mtc_repo" {
-  name        = "mtc-repo"
+  name        = "mtc-repo-${random_id.random.dec}"
   visibility  = "private"
   auto_init   = true
   description = "Code for mtc"
@@ -10,5 +14,13 @@ resource "github_repository_file" "readme" {
   branch              = "main"
   file                = "README.md"
   content             = "# This repository is for infra developers"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "index" {
+  repository          = github_repository.mtc_repo.name
+  branch              = "main"
+  file                = "index.html"
+  content             = "Hellow Terraform"
   overwrite_on_create = true
 }

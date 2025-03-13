09c7c38358e91cf0cfa44e7fd7cb4d6069b6b49f
Marcelo saad <marcelo_saad@hotmail.com>
2025-03-13 15:38:56 -0400

    add: create variable condition

--- secao_02/providers/variables.tf
+++ secao_02/providers/variables.tf
@@ -2,10 +2,25 @@ variable "repo_count" {
   default     = 1
   description = "Number of repositories."
   type        = number
+
+  validation {
+    condition     = var.repo_count < 5
+    error_message = "Do not deploy more than 5 repositories"
+  }
 }
 
-variable "varsource" {
+variable "env" {
   type        = string
-  description = "Source used to difine variables"
-  default     = "variables.tf"
+  description = "Deployment environment"
+  validation {
+    condition     = var.env == "dev" || var.env == "prod"
+    error_message = "Env must be 'dev' or 'prod'"
+  }
+
 }
+
+# variable "varsource" {
+#   type        = string
+#   description = "Source used to difine variables"
+#   default     = "variables.tf"
+# }
